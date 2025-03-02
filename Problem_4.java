1eb2ed4d97e31c48360cc608c36a63b0e3ae7956
DiMpho-20 <2816258@students.wits.ac.za>
2025-03-02 13:56:18 +0200

    Update Problem_4.java

--- Problem_4.java
+++ Problem_4.java
@@ -1,34 +1,34 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String args[]) {
-        List<String> students= new ArrayList<>();
-        Scanner scanner= new Scanner(System.in);
-        int num_students=scanner.nextInt();
-        for (int i=0; i<= num_students;i++){
-            String names= scanner.nextLine();
-            students.add(names);
 
-        }
-        for (int i= 0 ;i< num_students;i++)
-        {System.out.println(students.get(i));}
-            
+    public static int[] duplicate_finder(int[] my_array) {
+        Set<Integer> exists = new HashSet<>();
+        Set<Integer> duplees = new HashSet<>();
         
+        for (int num : my_array) {
+            
+            if (!exists.add(num)) {
+                // CHECKING whether the number is already in the array 
+                duplees.add(num);
+            }
+        }
 
+        
+        int[] result = new int[duplees.size()];
+        int index = 0;
+        for (int num : duplees) {
+            result[index] = num;
+            index++;
+        }
 
+        return result;
+    }
 
+    public static void main(String[] args) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-           }
+        int[] arr = {1, 2, 3, 2, 4, 5, 5};
+        int[] duplicates = duplicate_finder(arr);
+        System.out.println("Duplees: " +Arrays.toString(duplicates));
+    }
 }
